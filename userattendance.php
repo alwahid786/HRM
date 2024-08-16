d746eab0a62b912af9944cc02cb7db0cdb4bbc2d
arxllannn <147667804+arxllannn@users.noreply.github.com>
2024-08-16 00:19:21 -0700

    1 issue resolved

--- userattendance.php
+++ userattendance.php
@@ -139,9 +139,6 @@ if ($userType === 'admin') {
     </div>
 </section>
 
-<!-- Include jQuery and DataTables JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 
 <script>
     $(document).ready(function() {
